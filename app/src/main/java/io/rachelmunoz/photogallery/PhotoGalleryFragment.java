9b5e81e4a8c34991c304d16e95222001a29e20ea
rachelmunoz <rachelumunoz>
2017-09-27 10:49:12 -0700

    Adds IntentService to poll Flickr Api

--- app/src/main/java/io/rachelmunoz/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/io/rachelmunoz/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package io.rachelmunoz.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -46,6 +47,9 @@ public class PhotoGalleryFragment extends Fragment {
 		setRetainInstance(true);
 		setHasOptionsMenu(true);
 		updateItems(); // executes FlickrFetchr Api call in background thread
+
+		Intent i = PollService.newIntent(getActivity());
+		getActivity().startService(i);
 
 		Handler responseHandler = new Handler();
 		mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler); // pass in Handler attached to UI
---
+++ app/src/main/java/io/rachelmunoz/photogallery/PollService.java
@@ -0,0 +1,61 @@
+package io.rachelmunoz.photogallery;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by rachelmunoz on 9/26/17.
+ */
+
+public class PollService extends IntentService {
+	private static final String TAG = "PollService";
+
+	public static Intent newIntent(Context context){
+		return new Intent(context, PollService.class);
+	}
+
+	public PollService() {
+		super(TAG);
+	}
+
+	@Override
+	protected void onHandleIntent(@Nullable Intent intent) {
+		if(!isNetworkAvailableAndConnected()) return;
+
+		String query = QueryPreferences.getStoredQuery(this);
+		String lastResultId = QueryPreferences.getLastResultId(this);
+		List<GalleryItem> items;
+
+		if (query == null){
+			items = new FlickrFetchr().fetchRecentPhotos();
+		} else {
+			items = new FlickrFetchr().searchPhotos(query);
+		}
+
+		if (items.size() == 0) return;
+
+		String resultId = items.get(0).getId();
+		if (resultId.equals(lastResultId)){
+			Log.i(TAG, "Got an old result " + resultId);
+		} else {
+			Log.i(TAG, "Got a new result " + resultId);
+		}
+
+		QueryPreferences.setLastResultId(this, resultId);
+	}
+
+	private boolean isNetworkAvailableAndConnected() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+		boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+		boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+		return isNetworkConnected;
+	}
+}
