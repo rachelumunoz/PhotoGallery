9d35e195cad7dbd4cea872d30f5bc2804c06f835
rachelmunoz <rachelumunoz>
2017-10-03 11:31:41 -0700

    Displays a ProgressBar on image load using the WebChromeClient

--- app/src/main/java/io/rachelmunoz/photogallery/PhotoPageFragment.java
+++ app/src/main/java/io/rachelmunoz/photogallery/PhotoPageFragment.java
@@ -3,11 +3,14 @@ package io.rachelmunoz.photogallery;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by rachelmunoz on 10/3/17.
@@ -18,6 +21,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
 	private Uri mUri;
 	private WebView mWebView;
+	private ProgressBar mProgressBar;
 
 	public static PhotoPageFragment newInstance(Uri uri){
 		Bundle args = new Bundle();
@@ -40,8 +44,29 @@ public class PhotoPageFragment extends VisibleFragment {
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
+		mProgressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
+		mProgressBar.setMax(100);
+
 		mWebView = (WebView) view.findViewById(R.id.web_view);
 		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebChromeClient(new WebChromeClient(){
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				if (newProgress == 100){
+					mProgressBar.setVisibility(View.GONE);
+				} else {
+					mProgressBar.setVisibility(View.VISIBLE);
+					mProgressBar.setProgress(newProgress);
+				}
+			}
+
+			@Override
+			public void onReceivedTitle(WebView view, String title) {
+				AppCompatActivity activity = (AppCompatActivity) getActivity();
+				activity.getSupportActionBar().setSubtitle(title);
+			}
+		});
+
 		mWebView.setWebViewClient(new WebViewClient());
 		mWebView.loadUrl(mUri.toString());
 
